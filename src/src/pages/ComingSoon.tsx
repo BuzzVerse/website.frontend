3699a6eef140fcabb8353777b50dfd8294b81792
Grzegorz Dziedzic <grzegorzdziedzic198@gmail.com>
2024-02-16 19:16:07 +0100

    feat(Main): :construction: Concept of the main page
    not responsive and Hardcode values

---
+++ src/src/pages/ComingSoon.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+import { TypeWriter } from '../components/TypeWriter/TypeWriter';
+
+enum TextToType {
+  COMING_SOON = 'BuzzVerse coming soon'
+}
+
+export function ComingSoon() {
+  const [isCompleted, setCompleted] = useState<boolean>(false);
+
+  return (
+    <div className="bg-primary h-screen w-screen flex flex-col justify-center items-center">
+      <div
+        className="bg-secondary h-full flex flex-col justify-center items-center"
+        style={{
+          minWidth: isCompleted ? '100%' : '0',
+          transition: 'min-width 0.2s ease-out 0s'
+        }}>
+        <div className="h-full flex flex-col justify-center items-center">
+          <TypeWriter
+            textToType={[TextToType.COMING_SOON]}
+            className="text-5xl font-bold text-tertiary tracking-widest uppercase"
+            onComplete={() => setCompleted(true)}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
