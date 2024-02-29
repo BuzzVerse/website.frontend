c7481a2e146df85aa9f27a653707b1cc78a4b43d
DziedzicGrzegorz <grzegorzdziedzic198@gmail.com>
2024-02-29 16:50:26 +0100

    feat(LandingPage): Just example fulfil landing area

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,9 +1,11 @@
-import { ThreeCards } from './components/ThreeCards';
+import { MainView } from '@pages/LandingPage/components/MainView.tsx';
+import { ThreeCards } from '@pages/LandingPage/components/ThreeCards.tsx';
 
 export function LandingPage() {
   return (
     <div className="h-full bg-cover bg-fixed relative">
-      <div className="flex justify-center h-full">
+      <div className="flex flex-col items-center h-full">
+        <MainView />
         <ThreeCards />
       </div>
     </div>
