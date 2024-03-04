d19d42684ecfc9e29fdae8e8a5a092e4f59ea67e
Julia Grześkowiak <79307611+JuGecko@users.noreply.github.com>
2024-03-04 18:56:22 +0100

    footer margins + shadow to the cards

--- src/pages/LandingPage/components/ThreeCards.tsx
+++ src/pages/LandingPage/components/ThreeCards.tsx
@@ -1,17 +1,13 @@
 export function ThreeCards() {
   return (
     <div className="relative h-[9%] w-full flex justify-center items-center bg-quaternary opacity-85">
-      <div className="absolute top-[15%] left-1/3 w-1/6 h-1/3 bg-secondary rounded-md opacity-75	"></div>
-      <div className="absolute right-[35%] top-1/4 w-1/5 h-1/3 bg-secondary rounded-md opacity-50	"></div>
-      <div className="absolute bottom-[35%] right-[45%] w-1/6 h-1/3 bg-quaternary rounded-md opacity-50	"></div>
+      <div className="absolute top-[15%] left-1/3 w-1/6 h-1/3 bg-secondary rounded-lg opacity-50 z-[-1] shadow-lg shadow-blue-400/90"></div>
+      <div className="absolute right-[35%] top-1/4 w-1/5 h-1/3 bg-primary rounded-lg opacity-60 z-[-1] shadow-lg shadow-orange-400/90"></div>
+      <div className="absolute bottom-[35%] right-[45%] w-1/6 h-1/3 bg-quaternary rounded-lg opacity-60 z-10 shadow-lg shadow-white-400/90"></div>
       <div className="text-center z-10 py-5">
         <h1 className="text-5xl font-bold tracking-widest uppercase text-secondary">
           BuzzVerse
         </h1>
-        <br />
-        <h4 className={'text-secondary'}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        </h4>
       </div>
     </div>
   );
